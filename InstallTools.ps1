9d31b64bf33732f911aba54c071503e908634c05
PrateekAmey <44193777+PrateekAmey@users.noreply.github.com>
2020-04-27 10:49:34 +0100

    Automating tools installation

---
+++ InstallTools.ps1
@@ -0,0 +1,6 @@
+# Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Install Software
+choco install visualstudiocode -y
+choco install X2Go -y 
