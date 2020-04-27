a11b8f5a2d9693f36798916b93995a1221e67827
PrateekAmey <44193777+PrateekAmey@users.noreply.github.com>
2020-04-27 16:31:28 +0100

    Update InstallTools.ps1

--- InstallTools.ps1
+++ InstallTools.ps1
@@ -5,14 +5,3 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.We
 choco install visualstudiocode -y
 choco install X2Go -y 
 choco install Putty -y
-
-#Create accounts
-Get-Command -Module  Microsoft.PowerShell.LocalAccounts
-$Password = "Test123"
-$Username = "Testuser1"
-$Fullname = "Test User" 
-$Description = "Test User"
-$UserGroup = "Users"
-New-LocalUser $Username -Description $Description -NoPassword
-Set-LocalUser -Name $Username -Fullname $Fullname
-Add-LocalGroupMember -Group $UserGroup -Member $Username
