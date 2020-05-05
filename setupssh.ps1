a0dec3213591c6183d7293b9735448b6bebc487b
PrateekAmey <44193777+PrateekAmey@users.noreply.github.com>
2020-05-05 12:12:00 +0000

    Update setupssh.ps1

--- setupssh.ps1
+++ setupssh.ps1
@@ -1,8 +1,10 @@
-# Install the OpenSSHUtils module to the server. This will be valuable when deploying user keys.
-Install-Module -Force OpenSSHUtils -Scope AllUsers
 
-Get-Service  -Name ssh-agent | Set-Servuce -StartupType Manual
+Get-Service  -Name ssh-agent | Set-Service -StartupType Manual
+mkdir ~\.ssh
+cd ~\.ssh
+
 Start-Service ssh-agent 
 
-cd ~\.ssh\
-ssh-keygen
+ssh-keygen -f id_rsa -t rsa -N  '""'
+echo '\n' | plink dsvmadmin@host -pw "password" "mkdir ~/.ssh; chmod 700 ~/.ssh; touch ~/.ssh/authorized_keys; chmod 600 ~/.ssh/authorized_keys"
+cat ~/.ssh/id_rsa.pub | plink dsvmadmin@host -pw password "cat >>  ~/.ssh/authorized_keys"   #Maybe worked"
