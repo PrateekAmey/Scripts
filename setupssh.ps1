8bf4657ae71681b3a2dfb787ac5f02f804d43a5f
PrateekAmey <44193777+PrateekAmey@users.noreply.github.com>
2020-05-06 13:46:23 +0100

    added for uploading key to keyvault

--- setupssh.ps1
+++ setupssh.ps1
@@ -7,4 +7,8 @@ Start-Service ssh-agent
 
 ssh-keygen -f id_rsa -t rsa -N  '""'
 echo '\n' | plink dsvmadmin@host -pw "password" "mkdir ~/.ssh; chmod 700 ~/.ssh; touch ~/.ssh/authorized_keys; chmod 600 ~/.ssh/authorized_keys"
-cat ~/.ssh/id_rsa.pub | plink dsvmadmin@host -pw password "cat >>  ~/.ssh/authorized_keys"   #Maybe worked"
+cat ~/.ssh/id_rsa.pub | plink dsvmadmin@host -pw password "cat >>  ~/.ssh/authorized_keys"   
+
+#Store in keyvault
+$secretvalue = ConvertTo-SecureString (Get-Content C:/Users/Jumpboxadmin/.ssh/id_rsa.pub -Raw) -AsPlainText -Force
+$secret = Set-AzKeyVaultSecret -VaultName "Vault Name" -Name "keyName" -SecretValue $secretvalue
