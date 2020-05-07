ef315f2a9b6de8e2fb9aa11a8d5f1c43091a1a18
PrateekAmey <44193777+PrateekAmey@users.noreply.github.com>
2020-05-07 10:38:35 +0100

    Update setupssh.ps1

--- setupssh.ps1
+++ setupssh.ps1
@@ -7,6 +7,7 @@ Start-Service ssh-agent
 
 ssh-keygen -f id_rsa -t rsa -N  '""'
 echo '\n' | plink dsvmadmin@host -pw "password" "mkdir ~/.ssh; chmod 700 ~/.ssh; touch ~/.ssh/authorized_keys; chmod 600 ~/.ssh/authorized_keys"
+echo 'y' | plink dsvmadmin@host -pw "password" "mkdir ~/.ssh; chmod 700 ~/.ssh; touch ~/.ssh/authorized_keys; chmod 600 ~/.ssh/authorized_keys"
 cat ~/.ssh/id_rsa.pub | plink dsvmadmin@host -pw password "cat >>  ~/.ssh/authorized_keys"   
 
 #Store in keyvault
